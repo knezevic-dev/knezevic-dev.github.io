7992285ca7e531ab3b0d10731bcd3ca052cd8899
knezevic-dev <miraknezeviccc@gmail.com>
2024-06-25 10:14:44 +0200

    API request with website

---
+++ api.js
@@ -0,0 +1,30 @@
+async function fetchData(){
+    const pokemonName = document.getElementById("nameOfPokemon").value.toLowerCase();
+    const fetchingAPI = [
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`),
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`),
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    ];
+    try{
+        const responses = await Promise.all(fetchingAPI);
+        if (!responses.every(response => response.ok)) {
+            throw new Error("Responses are not okay!");
+        }
+        const data = await Promise.all(responses.map(response => response.json()));
+
+        const pokemonData = data[0];
+        const pokemonImage = pokemonData.sprites?.front_default;
+        if (pokemonImage) {
+            const imageElement = document.getElementById("pokemonImage");
+            imageElement.src = pokemonImage;
+            imageElement.style.display = "block";
+        } else {
+            console.error("Image not found");
+        }
+        console.log(data);
+    }
+    catch(error){
+        console.error("Failed to fetch!")
+    };
+};
+fetchData();
